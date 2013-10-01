ccba795a1c3cd298523ee934ac291a78830bf51b
hakantorun <hakan.torun@imovasyon.com>
2013-10-01 16:32:15 +0300

    asd

--- index.php
+++ index.php
@@ -2,93 +2,17 @@
 <head>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="http://code.jquery.com/js/jquery.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
 
     <?php include 'config.php'; ?>
 
 </head>
+
 <body>
 
-<div class="row-fluid show-grid">
-    <div class="span2" >
-        <div class="bs-docs-example">
-            <div class="well" style="max-width: 340px; padding: 8px 0;">
-                <ul class="nav nav-list">
-                    <li class="nav-header">List header</li>
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">Library</a></li>
-                    <li><a href="#">Applications</a></li>
-                    <li class="nav-header">Another list header</li>
-                    <li><a href="#">Profile</a></li>
-                    <li><a href="#">Settings</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">Help</a></li>
-                </ul>
-            </div> <!-- /well -->
-
-
-
-
-
-    </div>
-    <div class="span8">
-        <div class="row-fluid">
-
-                <li class="span4">
-                    <div class="thumbnail">
-                        <img src="https://static.garmincdn.com/en/products/010-00697-01/g/cf-lg.jpg" alt="">
-                        <div class="caption">
-                            <h3>Cep Telefonu</h3>
-                            <p>Mukemmel kucuklukte supersonik cep telefonu. Bu fiyata KACMAZZZZ !!!! </p>
-                            <p><a href="#" class="btn btn-primary">Incele</a> <a href="#" class="btn">Satin Al</a></p>
-                        </div>
-                    </div>
-                </li>
-            <li class="span4">
-                <div class="thumbnail">
-                    <img src="https://static.garmincdn.com/en/products/010-00697-01/g/cf-lg.jpg" alt="">
-                    <div class="caption">
-                        <h3>Cep Telefonu</h3>
-                        <p>Mukemmel kucuklukte supersonik cep telefonu. Bu fiyata KACMAZZZZ !!!! </p>
-                        <p><a href="#" class="btn btn-primary">Incele</a> <a href="#" class="btn">Satin Al</a></p>
-                    </div>
-                </div>
-            </li>
-            <li class="span4">
-                <div class="thumbnail">
-                    <img src="https://static.garmincdn.com/en/products/010-00697-01/g/cf-lg.jpg" alt="">
-                    <div class="caption">
-                        <h3>Cep Telefonu</h3>
-                        <p>Mukemmel kucuklukte supersonik cep telefonu. Bu fiyata KACMAZZZZ !!!! </p>
-                        <p><a href="#" class="btn btn-primary">Incele</a> <a href="#" class="btn">Satin Al</a></p>
-                    </div>
-                </div>
-            </li>
-            <li class="span4">
-                <div class="thumbnail">
-                    <img src="https://static.garmincdn.com/en/products/010-00697-01/g/cf-lg.jpg" alt="">
-                    <div class="caption">
-                        <h3>Cep Telefonu</h3>
-                        <p>Mukemmel kucuklukte supersonik cep telefonu. Bu fiyata KACMAZZZZ !!!! </p>
-                        <p><a href="#" class="btn btn-primary">Incele</a> <a href="#" class="btn">Satin Al</a></p>
-                    </div>
-                </div>
-            </li>
-
-
-        </div>
-
-
-
-
-    </div>
-
-    <div class="span2">
-
-
-
-
-    </div>
 
 
 </body>
